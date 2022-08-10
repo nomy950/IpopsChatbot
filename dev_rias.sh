de7e2d143e98fcc2e1b629258a81425f6fb923b7
nomy950 <noumannsyed@gmail.com>
2022-08-10 15:56:44 -0500

    Update dev_rias.sh

--- dev_rias.sh
+++ dev_rias.sh
@@ -4,5 +4,5 @@
 ibmcloud login --apikey $1 -r kr-seo
 clusterName=`ibmcloud ks clusters | grep $2 | grep $3 | awk '{ print $1 }'`
 ibmcloud ks cluster config -c $clusterName
-sh create-jumpod.sh
+sh create-jupod.sh
 #kubectl get pods -A

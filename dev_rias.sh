725dd9774300bcf3894600aaeccfb9717cc54584
nomy950 <noumannsyed@gmail.com>
2022-08-10 11:37:15 -0500

    Update dev_rias.sh

--- dev_rias.sh
+++ dev_rias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # your script here
 
-ibmcloud login --apikey $0 -r kr-seo
+ibmcloud login --apikey $1 -r kr-seo
 ibmcloud ks cluster config -c rias-ng-us-south-dal12-preprod
 kubectl get pods -A

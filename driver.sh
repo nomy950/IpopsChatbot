4558b089663409c1e01747636d8d18286bf3df2c
nomy950 <noumannsyed@gmail.com>
2022-08-15 17:19:28 -0500

    committing files

--- driver.sh
+++ driver.sh
@@ -1,6 +1,6 @@
     #! /bin/bash
     set -u
-    sudo sh dev_rias_78.sh ${d_api_key}
+    sudo sh rias_scripts/dev_rias_79.sh ${d_api_key}
     export JOB_LOG="$PIPELINE_LOG_URL"
     echo ${JOB_LOG}
     

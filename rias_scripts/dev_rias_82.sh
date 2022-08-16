f8c4ac6d12bdf52d9b4e6704c5cf15641969c5dd
nomy950 <noumannsyed@gmail.com>
2022-08-16 12:41:10 -0500

    Delete rias_scripts directory

--- rias_scripts/dev_rias_82.sh
+++
@@ -1,6 +0,0 @@
-    #! /bin/bash
-    ibmcloud login --apikey $1 -r kr-seo
-    clusterName=`ibmcloud ks clusters | grep dal12 | grep preprod | awk '{ print $1 }'`
-    ibmcloud ks cluster config -c $clusterName
-    kubectl get pods -A
-    

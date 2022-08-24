6d59b7dc9ae7247b7d07b34bc3b4fa1a2f24e1e1
nomy950 <noumannsyed@gmail.com>
2022-08-24 15:36:31 -0500

    committing files

---
+++ rias_scripts/dev_rias_85.sh
@@ -0,0 +1,8 @@
+    #! /bin/bash
+    ibmcloud login --apikey $1 -r kr-seo
+    clusterName=`ibmcloud ks clusters | grep dal11 | grep preprod | awk '{ print $1 }'`
+    ibmcloud ks cluster config -c $clusterName
+    echo "<Logs_Start>"
+    kubectl get pods -A
+    echo "<Logs_End>"
+    
